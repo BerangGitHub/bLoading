9180f14174774667b081e7c181ae061698709e60
Berang <berang0108@gmail.com>
2023-11-21 20:04:43 +0100

    fxmanifest credit

--- bLoading/fxmanifest.lua
+++ bLoading/fxmanifest.lua
@@ -1,6 +1,10 @@
 fx_version "cerluan"
 game "gta5"
 
+repository 'https://github.com/BerangGitHub/bLoading'
+author 'Berang'
+description 'Loading Screen by berang on Discord'
+
 loadscreen 'html/index.html'
 
 files {
